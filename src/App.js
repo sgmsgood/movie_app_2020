d32aee54ed143cfcfabe582a71e41abe74efbb92
sgmsgood <sgms_good@naver.com>
2020-09-24 17:28:59 +0900

    1.구조 분해 할당 props 2.map 함수 사용하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+function Food({name, picture}) {
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <img src={picture}/>
+  </div>
+  )
+
+}
+
+const foodILike = [
+  {
+    name: 'Gopchang',
+    image: 'https://postfiles.pstatic.net/20150804_178/s__sseunga_1438656361978V6eRo_JPEG/IMG_7859.jpg?type=w2'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://postfiles.pstatic.net/MjAxOTEyMjdfNzQg/MDAxNTc3MzgxMDQwOTEw.qtYADZhlPIHnFUSICv7FwSiwic1SPOUGDU4F16_EaZ8g.RQHHJyMrWtOOXIgr0QGGv-uhQDeETIP5eSL-Xjbp3qwg.JPEG.baboo2080/19.JPG?type=w966'
+  }
+];
+
 function App() {
-  return <div>Hallo@!!!</div>;
+  return( 
+  <div>
+    <h1> Hallo@!!! </h1>
+    {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image}/>
+    ))}
+  </div>
+    );
 }
 
 export default App;
