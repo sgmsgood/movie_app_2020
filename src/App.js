8df3bf4e73059790b03c6157c8b790262fa0c045
sgmsgood <45481968+sgmsgood@users.noreply.github.com>
2020-10-02 08:13:51 +0900

    loding page 구현
    1. setTimeout() 구현

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
   state = {
-    count: 0,
+    isLoding: true,
+    movies: [],
   };
 
-  add = () => {
-    // console.log('add');
-    // this.state.count = 1; //사용 불가능
-    // this.setState({count: this.state.count + 1})
-    this.setState(current => ({
-      count: current.count + 1,
-    }))
-  }
-
-  minus = () => {
-    // console.log('minus');
-    // this.state.count = -1;
-    // this.setState({count: this.state.count - 1})
-    this.setState(current => ({
-      count: current.count - 1,
-    }))
-  }
-
   componentDidMount() {
-    console.log('component rendered');
+    setTimeout(() => {
+      this.setState({ isLoding: false});
+    }, 6000);
   }
-
-  componentDidUpdate() {
-    console.log('I just updated');
-  }
-
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world');
-  }
-
-  render() {
-    console.log('render');
-    return (
-      <div>
-        <h1> The number is: {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
-  }
+ render() {
+   const {isLoding} = this.state;
+   return <div>{isLoding? 'Loding...' : 'We are ready'}</div>
+ }
 }
 
 export default App;
