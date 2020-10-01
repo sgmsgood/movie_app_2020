92a0b96667a6cc021032d0a4f2e7d3d407b5aacb
sgmsgood <45481968+sgmsgood@users.noreply.github.com>
2020-10-02 05:02:59 +0900

    리액트 기본개념
    1. 동적 setState()사용
    2. 리액트 생명주기

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-  )
-
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Gopchang',
-    image: 'https://postfiles.pstatic.net/20150804_178/s__sseunga_1438656361978V6eRo_JPEG/IMG_7859.jpg?type=w2',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://postfiles.pstatic.net/MjAxOTEyMjdfNzQg/MDAxNTc3MzgxMDQwOTEw.qtYADZhlPIHnFUSICv7FwSiwic1SPOUGDU4F16_EaZ8g.RQHHJyMrWtOOXIgr0QGGv-uhQDeETIP5eSL-Xjbp3qwg.JPEG.baboo2080/19.JPG?type=w966',
-    rating: 4.9,
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
   }
-];
+  state = {
+    count: 0,
+  };
 
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />;
+  add = () => {
+    // console.log('add');
+    // this.state.count = 1; //사용 불가능
+    // this.setState({count: this.state.count + 1})
+    this.setState(current => ({
+      count: current.count + 1,
+    }))
+  }
+
+  minus = () => {
+    // console.log('minus');
+    // this.state.count = -1;
+    // this.setState({count: this.state.count - 1})
+    this.setState(current => ({
+      count: current.count - 1,
+    }))
+  }
+
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1> The number is: {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
-
-function App() {
-  return (
-    <div>
-      <h1> Hallo@!!! </h1>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
-      {/* {foodILike.map(renderFood)} */}
-    </div>
-  );
-}
-
-Food.prototype = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
