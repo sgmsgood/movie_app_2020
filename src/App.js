a2c2ca968db49ce3d0a1f039e34f097a01d92da4
sgmsgood <45481968+sgmsgood@users.noreply.github.com>
2020-10-03 11:12:31 +0900

    리스트 카드뷰처럼 수정

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movies';
+import Home from './routes/Home'
+import './App.css';
 
-class App extends React.Component {
-  state = {
-    isLoding: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const{
-      data: {
-        data: { movies },
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoding: false });
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
- render() {
-   const {isLoding, movies} = this.state;
-   return <div>{isLoding? 'Loding...' 
-   : movies.map((movie) => {
-     return (<Movie
-     id={movie.id}
-     year={movie.year}
-     title={movie.title}
-     summary={movie.summary}
-     poster={movie.medium_cover_image}
-     />
-     );
-   })}</div>
- }
+function App() {
+  return <Home/>;
 }
 
 export default App;
